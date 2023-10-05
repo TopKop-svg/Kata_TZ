2cf7a9bf87ca1c90db5a885a41def0c796b543c6
TopKop-svg <loookintome@gmail.com>
2023-10-05 21:06:15 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static String calc(String input) {
+        
+        return null;
+    }
+
+}
